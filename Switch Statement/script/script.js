1ce62bcb1b126d8bb2ccfb2145ec152876bb9e2c
ninjafiveo <42717162+ninjafiveo@users.noreply.github.com>
2022-01-31 10:33:00 -0500

    first commit

---
+++ Switch Statement/script/script.js
@@ -0,0 +1,25 @@
+//ToDo Global Variabls
+//! Declare the following global variables: subject, totalQuestions, numCorrect, percentScore
+
+
+//ToDo - New Function
+//! Declare a function named testResults(), this is called when the calculate button is clicked. 
+
+//ToDo - this goes inside the testResults() function
+    //? In the function you will need to set the value of the variables: subject, totalQUestions and numbCorrect.
+    //* Think document.getElementById().value;
+
+//ToDo - this goes inside the testResults() function
+    //? calculate percentScore by dividing numCorrect by totalQuestions... multiply by 100, so it's 96 instead of .96
+    //* limit (fix) the percentScore to two decimal spots so that it's 96.27 rather than 96.274382333333333
+
+//ToDo - this goes inside the testResults() function
+    //? Create 4 local variables. Minimum scores for an A, B, C & D (note letter grade F witll be anything less than a D. ) 
+
+//ToDo - this goes inside the testResults() function
+    //! Add Switch Statement that checks the condition of the grade. For each grade change the out of the html id's of "testResults" and "testMessage".
+    //? note you are just checking to see if a condition is true... so you may want to start with switch(true){}
+
+  
+//ToDo - New Function
+// Create a function called clearResults() that resets the forms values. 
